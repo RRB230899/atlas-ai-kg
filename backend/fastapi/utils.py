6b0150cabc5ef672afa88912be9fc749f2a1f7f9
RRB230899 <bajajraghav9@gmail.com>
2025-10-17 14:58:07 -0400

    Creds from env file

--- backend/fastapi/utils.py
+++ backend/fastapi/utils.py
@@ -1,14 +1,18 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
 
 # Load model once
 model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
+
+load_dotenv()
 def get_conn():
     return psycopg2.connect(
-        dbname="atlas_db",
-        user="atlas_user",
-        password="atlas_pass",
+        dbname=os.getenv("POSTGRES_DBNAME"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD"),
         host="localhost",
         port=5432
     )
